2cd9c2952e2c3851c8cbd1e43b480aecc64273b4
paulaallemand <paulaisdev@gmail.com>
2024-01-13 00:17:49 -0300

    updated routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,12 @@
-import UserController from '../controllers/UserController';
-import verifyToken from '../middlewares/authJwt';
+import UserController from "../controllers/UserController";
+import verifyToken from "../middlewares/authJwt";
 
 export default (app) => {
   // USER ROUTES
   //app.get(`/api`,)
-  app.get(`/api/user/all`, UserController.getAll);
+  app.get(`/api/user/`, UserController.getAll);
   app.get(`/api/user/:params`, UserController.get);
-  app.post(`/api/user/create`, [verifyToken], UserController.insert)
+  app.post(`/api/user/`, [verifyToken], UserController.insert);
   app.put(`/api/user/:id`, [verifyToken], UserController.update);
   app.delete(`/api/user/:id`, [verifyToken], UserController.delete);
-}
+};
